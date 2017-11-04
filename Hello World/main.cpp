b2e25d853bef8a174aaffc0134818dd02f2a82c1
André Tonic <andre_tonic@iMac-de-Andre-Tonic.local>
2017-11-04 09:05:56 +0100

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by André Tonic on 04/11/2017.
+//  Copyright © 2017 André Tonic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
