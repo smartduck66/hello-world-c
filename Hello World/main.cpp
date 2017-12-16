010af3b62322ac76053081b47562977bedfbc259
André Tonic <andre_tonic@imac-de-andre-tonic.local>
2017-12-16 11:05:10 +0100

    Exercice terminé 218.4

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -6,7 +6,7 @@
 // #include <iostream>
 // #include <math.h>
 
-// Caractères spéciaux : [ ]   '\n'   {  }   ||
+// Caractères spéciaux : [ ]   '\n'   {  }   ||    Bool : 0 Faux 1 Vrai
 
 // Nouvelle version de l'exercice 19 - Chap 4 - page 130 - Création d'une class Name
 
@@ -15,9 +15,9 @@
 #include "std_lib_facilities.h"
 
 
-// -----------------------------------------------------------------------------------------------------
+// *****************************************************************************************************
 // Exo 4 - page 218 = nouvelle version de l'exercice 19 - Chap 4 - page 130 - Création d'une class Name
-// -----------------------------------------------------------------------------------------------------
+// *****************************************************************************************************
 
 class Name_value {
 public:
@@ -28,10 +28,27 @@ public:
 };
 
 
+class Name_value_exist{ // On crée une classe avec une seule méthode qui permet de vérifier l'existence d'une occurence dans un vecteur
+public:
+    bool exist(vector<Name_value> p, string n, int v);
+};
+
+Name_value_exist e; // On fournit la méthode exist
+
+bool Name_value_exist::exist(vector<Name_value> p, string n, int v) // Définition de la méthode exist
+{
+    // On balaye le vecteur pour vérifier
+    for (Name_value x:p)
+        if (x.name==n&&x.value==v)return 1; // la paire existe déjà
+    return 0; // la paire n'existe pas
+}
+
+
+// -----------------------------------------------------------------------------------------------------
 int main()
 {
 
-    vector<Name_value> paire;
+    vector<Name_value> paire; // Création du vecteur avec la classe définie plus haut
     string nom="";
     int valeur=0;
     
@@ -42,16 +59,18 @@ int main()
         if (nom=="NoName"&&valeur==0)break;
         
         // Vérification que cette nouvelle paire saisie n'a pas DEJA été saisie
-        if (ExistePaire) {
+        if (e.exist(paire,nom,valeur)) {
             cout << "Cette paire a déjà été saisie. Veuillez recommencer\n";
         } else {
             // Si ce n'est pas le cas, on push
-            Name_value(nom,valeur);
+            paire.push_back(Name_value(nom,valeur));
         }
     
     }
     
-    // Affichage de toutes les paires saisies en balayant le vecteur
+    // Affichage de toutes les paires saisies en balayant le vecteur après un tri
+    // Attention : le tri sort(paire) ne fonctionne pas - Il faudra vérifier plus tard quelle syntaxe de tri sur un vecteur construit autour d'un type particulier (nom, valeur)
+    cout << "Le vecteur comprend "<<paire.size()<<" éléments\n";
     for (Name_value x:paire)
         cout << x.name << " - "<<x.value<<'\n';
         
